67f61b6184c5e6de57e35a70c49456813d3ceda2
Ilinca Tudose <ilinca.tudose@gmail.com>
2020-03-21 22:30:18 +0100

    Mock functions so you have the signature for the going out decision method.
    I'll do the implementation tomorrow.

---
+++ src/helper_functions.js
@@ -0,0 +1,7 @@
+function canGoOut(city, state, activity, number_of_people, time) {
+  return Math.random() > 0.5;
+}
+
+function getActivityList(place_type){
+  return ["Freizeiteinrichtungen", "Schulen", "Lieferdienste"]
+}
