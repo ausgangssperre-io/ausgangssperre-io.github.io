4a0cbf21c456aaf31c6fa595454b4622a24981ea
Bastian Haustein <bastian.haustein@viator-group.de>
2020-03-22 13:14:58 +0100

    deployscript working but we need to change the offset of start and end tag because of body class and the active nav item changes from page to page.

--- deploy-header-and-footer.php
+++ deploy-header-and-footer.php
@@ -20,7 +20,8 @@ $footer = file_get_contents('src/partial-footer.html');
 foreach ($files as $file) {
     $content = file_get_contents($file);
     preg_match_all('/<!-- CONTENT START -->(.*)<!-- CONTENT END -->/s', $content, $matches, PREG_SET_ORDER, 0);
-    $content = $matches[1];
+    $content = $matches[0] ?? [];
+    $content = $content[1] ?? [];
     if(empty($content)) {
         echo "{$file} hat <!-- CONTENT START --> oder <!-- CONTENT END --> nicht richtig implementiert.\n";
         continue;
